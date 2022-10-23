53bf0d42e62e57d21895b25e6a892f582c85838e
“mtstechnologies” <michael.trindade@hotmail.com>
2022-10-23 18:13:07 -0400

    Projeto entregas criado

---
+++ src/main/java/com/mts/entrega/MtsEntregaApiApplication.java
@@ -0,0 +1,13 @@
+package com.mts.entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsEntregaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsEntregaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mts/entrega/MtsEntregaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsEntregaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
